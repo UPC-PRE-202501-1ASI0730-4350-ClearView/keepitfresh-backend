581152173a6fa4472946d8ce941d697b0efff93a
Werner Lang <u202310003@upc.edu.pe>
2025-06-11 13:38:37 -0500

    feat(Order): add Dish entity

---
+++ KeepItFresh.Platform.API/Order/Domain/Model/Entities/Dish.cs
@@ -0,0 +1,12 @@
+namespace KeepItFresh.Platform.API.Order.Domain.Model.Entities;
+
+public class Dish
+{
+    public int Id { get; }
+    
+    public string Name { get; set; }
+    
+    public int Price { get; set; }
+    
+    
+}
