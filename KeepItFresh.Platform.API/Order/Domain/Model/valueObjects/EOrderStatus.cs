abb6ff4139e4d17e756eb2538c24abaa2fd7c631
Werner Lang <u202310003@upc.edu.pe>
2025-06-11 13:30:59 -0500

    feat(Order): add EOrderStatus value object

---
+++ KeepItFresh.Platform.API/Order/Domain/Model/valueObjects/EOrderStatus.cs
@@ -0,0 +1,13 @@
+namespace KeepItFresh.Platform.API.Order.Domain.Model.valueObjects;
+
+/// <summary>
+/// Represents the status of an Order.
+/// </summary>
+
+public enum EOrderStatus
+{
+    Pending,
+    Preparing,
+    Ready,
+    Completed
+}
