fc22fd8a4deeae79d3a49892b0101f11f84a9977
Werner Lang <U202310003@upc.edu.pe>
2025-06-16 11:01:38 -0500

    feat(orders): add OrderRepository

---
+++ KeepItFresh.Platform.API/Order/Domain/Repositories/IOrderRepository.cs
@@ -0,0 +1,8 @@
+using ACME.LearningCenterPlatform.API.Shared.Domain.Repositories;
+
+namespace KeepItFresh.Platform.API.Order.Domain.Repositories;
+
+public interface IOrderRepository : IBaseRepository<Model.Aggregates.Order>
+{
+    
+}
