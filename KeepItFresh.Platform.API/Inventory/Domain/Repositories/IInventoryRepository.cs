e51be9b4e0aeeef8333dc3c26e24ed7745ab9b2c
Pedro Guia <u202212010@upc.edu.pe>
2025-06-16 23:46:57 -0500

    feat(inventory): add interface inventory repository.

---
+++ KeepItFresh.Platform.API/Inventory/Domain/Repositories/IInventoryRepository.cs
@@ -0,0 +1,10 @@
+using ACME.LearningCenterPlatform.API.Shared.Domain.Repositories;
+
+namespace KeepItFresh.Platform.API.Inventory.Domain.Repositories;
+
+public interface IInventoryRepository : IBaseRepository<Model.Aggregates.Inventory>
+{
+    Task<Model.Aggregates.Inventory?> findByNameAync(string name);
+    Task<IEnumerable<Model.Aggregates.Inventory>> findByProductIdAsync(int productId);
+    Task<IEnumerable<Model.Aggregates.Inventory>> findBySupplierIdAsync(int supplierId);
+}
