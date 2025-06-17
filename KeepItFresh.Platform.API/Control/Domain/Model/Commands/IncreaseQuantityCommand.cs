af8da198cf920f2e8416e206f857b8a13758be5c
Pedro Guia <u202212010@upc.edu.pe>
2025-06-17 00:33:33 -0500

    feat(control): update increase quantity command.

--- KeepItFresh.Platform.API/Control/Domain/Model/Commands/IncreaseQuantityCommand.cs
+++ KeepItFresh.Platform.API/Control/Domain/Model/Commands/IncreaseQuantityCommand.cs
@@ -1,3 +1,3 @@
 namespace KeepItFresh.Platform.API.Control.Domain.Model.Commands;
 
-public record IncreaseQuantityCommand(int Quantity, int ProductId);
+public record IncreaseQuantityCommand(int Quantity, int InventoryId);
