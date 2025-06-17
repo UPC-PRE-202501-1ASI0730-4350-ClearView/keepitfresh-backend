424d7fd21373faa7556363171db6f8d7f2988ece
Pedro Guia <u202212010@upc.edu.pe>
2025-06-17 00:15:06 -0500

    feat(control): add interface inventory repository.

---
+++ KeepItFresh.Platform.API/Control/Infrastructure/Persistence/EFC/Repositories/InventoryRepository.cs
@@ -0,0 +1,26 @@
+using ACME.LearningCenterPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using KeepItFresh.Platform.API.Control.Domain.Model.Aggregates;
+using KeepItFresh.Platform.API.Control.Domain.Repositories;
+using KeepItFresh.Platform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace KeepItFresh.Platform.API.Control.Infrastructure.Persistence.EFC.Repositories;
+
+public class InventoryRepository(AppDbContext context) 
+    : BaseRepository<Inventory>(context), IInventoryRepository
+{
+    public async Task<Inventory?> findByNameAync(string name)
+    {
+        return await Context.Set<Inventory>().FirstOrDefaultAsync(p => p.Name == name);
+    }
+
+    public async Task<IEnumerable<Inventory>> findByProductIdAsync(int productId)
+    {
+        return await Context.Set<Inventory>().Where(p => p.ProductId == productId).ToListAsync();
+    }
+
+    public async Task<IEnumerable<Inventory>> findBySupplierIdAsync(int supplierId)
+    {
+        return await Context.Set<Inventory>().Where(p => p.SupplierId == supplierId).ToListAsync();
+    }
+}
