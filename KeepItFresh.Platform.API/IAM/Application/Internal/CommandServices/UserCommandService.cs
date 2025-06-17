ca2addade34bb83d459643043055c7964fb7284f
Ayrton <u202311077@upc.edu.pe>
2025-06-16 19:09:37 -0500

    feat(IAM): add command services

---
+++ KeepItFresh.Platform.API/IAM/Application/Internal/CommandServices/UserCommandService.cs
@@ -0,0 +1,17 @@
+using KeepItFresh.Platform.API.IAM.Application.Internal.OutboundServices;
+using KeepItFresh.Platform.API.IAM.Domain.Repositories;
+using KeepItFresh.Platform.API.IAM.Domain.Services;
+using KeepItFresh.Platform.API.Shared.Domain.Repositories;
+
+namespace KeepItFresh.Platform.API.IAM.Application.Internal.CommandServices;
+
+public class UserCommandService(
+    IUserRepository userRepository,
+    ITokenService tokenService,
+    IHashingService hashingService,
+    IUnitOfWork unitOfWork,
+    ExternalProfileService externalProfileService
+    ) 
+{
+    
+}
