79ee5160a6ed3342919b353f3b9d57d68d47bf01
Werner Lang <U202310003@upc.edu.pe>
2025-06-18 10:10:00 -0500

    feat(Order): Add Order Repository tasks

--- KeepItFresh.Platform.API/Order/Infrastructure/Persistence/EFC/Repositories/OrderRepository.cs
+++ KeepItFresh.Platform.API/Order/Infrastructure/Persistence/EFC/Repositories/OrderRepository.cs
@@ -2,14 +2,20 @@ using KeepItFresh.Platform.API.Order.Domain.Model.Aggregates;
 using ACME.LearningCenterPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
 using KeepItFresh.Platform.API.Order.Domain.Repositories;
 using KeepItFresh.Platform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace KeepItFresh.Platform.API.Order.Infrastructure.Repositories;
 
 public class OrderRepository(AppDbContext context)
     : BaseRepository<Orders>(context), IOrderRepository
 {
-    public Task<IEnumerable<Orders>> FindByNameAsync(string name)
+    public async Task<IEnumerable<Orders>> FindByNameAsync(string name)
     {
-        throw new NotImplementedException();
+        return await Context.Set<Orders>().Where(p => p.Name == name).ToListAsync();
+    }
+
+    public async Task<IEnumerable<Orders>?> FindByIdAsync(int id)
+    {
+        return await Context.Set<Orders>().Where(p => p.Id == id).ToListAsync();
     }
 }
