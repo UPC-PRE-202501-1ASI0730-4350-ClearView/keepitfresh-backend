8b55ed20a38afc11382ce2bb90e8fb11392d7f37
Werner Lang <u202310003@upc.edu.pe>
2025-06-17 19:53:06 -0500

    feat(orders): add Orders content

--- KeepItFresh.Platform.API/Order/Domain/Model/Aggregates/Orders.cs
+++ KeepItFresh.Platform.API/Order/Domain/Model/Aggregates/Orders.cs
@@ -2,16 +2,12 @@ using KeepItFresh.Platform.API.Order.Domain.Model.Commands;
 
 namespace KeepItFresh.Platform.API.Order.Domain.Model.Aggregates;
 
-public class Orders
+public partial class Orders
 {
     public int Id { get; }
     public string Name { get; private set; }
     public List<Dish> Dishes { get; private set; } = new List<Dish>();
 
-    public Orders()
-    {
-        Name = string.Empty;
-    }
     
     public Orders(string name)
     {
---
+++ KeepItFresh.Platform.API/Order/Domain/Model/Aggregates/OrdersContent.cs
@@ -0,0 +1,36 @@
+﻿using KeepItFresh.Platform.API.Order.Domain.Model.ValueObjects;
+
+namespace KeepItFresh.Platform.API.Order.Domain.Model.Aggregates;
+
+public partial class Orders : IStatus
+{
+    
+    public EOrderStatus Status { get; protected set; }
+
+    public Orders()
+    {
+        Name = string.Empty;
+    }
+    
+    
+    
+    public void SendToPrepare()
+    {
+        Status = EOrderStatus.Preparing;
+    }
+
+    public void SendToReady()
+    {
+        Status = EOrderStatus.Ready;
+    }
+
+    public void SendToCompleted()
+    {
+        Status = EOrderStatus.Completed;
+    }
+
+    public void SendToCancelled()
+    {
+        Status = EOrderStatus.Canceled;
+    }
+}
