6417b56017d9711b920c5e55b7e52aceb61012bc
Marco Antonio Nakasone Gomes <marcoanakasone@gmail.com>
2025-06-18 01:13:15 -0500

    feat(sensor): implement InventoryHttpGateway for product management

---
+++ KeepItFresh.Platform.API/Sensor/Infrastructure/Persistance/EFC/Repositories/InventoryHttpGateway.cs
@@ -0,0 +1,23 @@
+﻿using KeepItFresh.Platform.API.Sensor.Domain.Services;
+
+namespace KeepItFresh.Platform.API.Sensor.Infrastructure.Persistance.EFC.Repositories;
+
+public class InventoryHttpGateway : IInventoryGateway
+{
+    private readonly HttpClient _httpClient;
+
+    public InventoryHttpGateway(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<List<ProductDto>> GetAllProductsAsync()
+    {
+        return await _httpClient.GetFromJsonAsync<List<ProductDto>>("http://localhost:3000/products") ?? new();
+    }
+
+    public async Task UpdateProductAsync(int productId, ProductDto product)
+    {
+        await _httpClient.PutAsJsonAsync($"http://localhost:3000/products/{productId}", product);
+    }
+}
