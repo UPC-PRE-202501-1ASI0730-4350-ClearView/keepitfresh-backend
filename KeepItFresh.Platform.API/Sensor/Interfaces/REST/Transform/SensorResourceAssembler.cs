35c1a217a1814502257888766cb0aa5a0dc99a92
Marco Antonio Nakasone Gomes <marcoanakasone@gmail.com>
2025-06-18 01:13:50 -0500

    feat(sensor): add SensorResourceAssembler for transforming SensorData to SensorResource

---
+++ KeepItFresh.Platform.API/Sensor/Interfaces/REST/Transform/SensorResourceAssembler.cs
@@ -0,0 +1,13 @@
+﻿using KeepItFresh.Platform.API.Sensor.Domain.Services;
+using KeepItFresh.Platform.API.Sensor.Interfaces.REST.Resources;
+
+namespace KeepItFresh.Platform.API.Sensor.Interfaces.REST.Transform;
+
+public static class SensorResourceAssembler
+{
+    public static SensorResource ToResource(ProductDto.SensorData sensor) => new()
+    {
+        Type = sensor.Type,
+        Status = sensor.Status
+    };
+}
