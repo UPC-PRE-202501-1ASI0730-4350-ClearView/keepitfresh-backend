838f57af1026d972019ac1ad11b88d9976ff8ba3
Ayrton <u202311077@upc.edu.pe>
2025-06-18 11:45:23 -0500

    fix: Restructured code for ExternalProfileService

--- KeepItFresh.Platform.API/IAM/Application/Internal/OutboundServices/ExternalProfileService.cs
+++ KeepItFresh.Platform.API/IAM/Application/Internal/OutboundServices/ExternalProfileService.cs
@@ -1,11 +1,6 @@
-using KeepItFresh.Platform.API.IAM.Application.ACL;
-
 namespace KeepItFresh.Platform.API.IAM.Application.Internal.OutboundServices;
 
-public class ExternalProfileService(IProfilesContextFacade profilesContextFacade)
+public class ExternalProfileService()
 {
-    public async Task<int> CreateProfile(string fullName, string email, string userId)
-    {
-        return await profilesContextFacade.CreateProfileAsync(fullName, email, userId);
-    }
+
 }
