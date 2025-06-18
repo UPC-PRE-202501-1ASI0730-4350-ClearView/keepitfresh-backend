f561ec9eccedadb51255441a713de04302d07afe
Werner Lang <u202310003@upc.edu.pe>
2025-06-17 22:55:56 -0500

    feat(Order): add Create Dish Command From Resource Assembler

---
+++ KeepItFresh.Platform.API/Order/interfaces/REST/Transform/CreateDishCommandFromResourceAssembler.cs
@@ -0,0 +1,14 @@
+﻿using KeepItFresh.Platform.API.Order.Domain.Model.Commands;
+using KeepItFresh.Platform.API.Order.interfaces.REST.Resources;
+
+namespace KeepItFresh.Platform.API.Order.interfaces.REST.Transform;
+
+public class CreateDishCommandFromResourceAssembler
+{
+    public static CreateDishCommand ToCommandFromResource(CreateDishResource resource)
+    {
+        return new CreateDishCommand(
+            resource.Name,
+            resource.Price);
+    }
+}
