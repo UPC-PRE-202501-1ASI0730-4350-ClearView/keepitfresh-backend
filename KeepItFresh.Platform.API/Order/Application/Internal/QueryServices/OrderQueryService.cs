54cf5f1502a32cf5eb9e7098d8d8142a455381f7
Werner Lang <u202310003@upc.edu.pe>
2025-06-17 19:57:24 -0500

    feat(orders): add Orders Query Service

---
+++ KeepItFresh.Platform.API/Order/Application/Internal/QueryServices/OrderQueryService.cs
@@ -0,0 +1,19 @@
+﻿using KeepItFresh.Platform.API.Order.Domain.Model.Aggregates;
+using KeepItFresh.Platform.API.Order.Domain.Model.Queries;
+using KeepItFresh.Platform.API.Order.Domain.Repositories;
+using KeepItFresh.Platform.API.Order.Domain.Services;
+
+namespace KeepItFresh.Platform.API.Order.Application.Internal.QueryServices;
+
+public class OrderQueryService(IOrderRepository repository) : IOrderQueryServices
+{
+    public async Task<IEnumerable<Orders>> Handle(GetAllOrdersQuery query)
+    {
+        return await repository.ListAsync();
+    }
+
+    public async Task<Orders?> Handle(GetOrderByIdQuery query)
+    {
+        return await repository.FindByIdAsync(query.id);
+    }
+}
