bac90a924b1fe82e6e605130c9a767aa9de9c7e9
Marco Antonio Nakasone Gomes <marcoanakasone@gmail.com>
2025-06-18 21:35:17 -0500

    feat(sensor): add SensorData value object for sensor attributes

---
+++ KeepItFresh.Platform.API/Inventory/Domain/Model/ValueObjects/SensorData.cs
@@ -0,0 +1,15 @@
+﻿namespace KeepItFresh.Platform.API.Inventory.Domain.Model.ValueObjects;
+
+public class SensorData
+{
+    public string Id { get; private set; }
+    public string Type { get; private set; }
+    public string Status { get; private set; }
+
+    public SensorData(string id, string type, string status)
+    {
+        Id = id;
+        Type = type;
+        Status = status;
+    }
+}
