69d29e659401221c8456eb81cadd5e0239a1d4cf
Marco Antonio Nakasone Gomes <marcoanakasone@gmail.com>
2025-06-18 21:35:38 -0500

    feat(product): add UpdateProductCommand for product updates

---
+++ KeepItFresh.Platform.API/Inventory/Domain/Model/Commands/UpdateProductCommand.cs
@@ -0,0 +1,12 @@
+﻿using KeepItFresh.Platform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace KeepItFresh.Platform.API.Inventory.Domain.Model.Commands;
+
+public record UpdateProductCommand( int Id,
+    string Name,
+    string Category,
+    int Quantity,
+    decimal Price,
+    DateTime ExpirationDate,
+    string? Image,
+    SensorData? Sensor);
