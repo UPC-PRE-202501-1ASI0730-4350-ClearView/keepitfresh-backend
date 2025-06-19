370ec1d80c43bf5b8ceb39bf5324d28764b36f5e
Marco Antonio Nakasone Gomes <marcoanakasone@gmail.com>
2025-06-18 21:34:46 -0500

    feat(inventory): configure database context and CORS policy for inventory services

--- KeepItFresh.Platform.API/Program.cs
+++ KeepItFresh.Platform.API/Program.cs
@@ -1,11 +1,16 @@
+using KeepItFresh.Platform.API.Inventory.Application.Internal.CommandServices;
+using KeepItFresh.Platform.API.Inventory.Application.Internal.QueryServices;
+using KeepItFresh.Platform.API.Inventory.Domain.Repositories;
+using KeepItFresh.Platform.API.Inventory.Infrastructure.Persistence.EFC;
+using KeepItFresh.Platform.API.Inventory.Infrastructure.Persistence.EFC.Repositories;
 using KeepItFresh.Platform.API.Sensor.Application.Internal.CommandServices;
+using KeepItFresh.Platform.API.Sensor.Application.Internal.QueryServices;
 using KeepItFresh.Platform.API.Sensor.Domain.Services;
 using KeepItFresh.Platform.API.Sensor.Infrastructure.Persistance.EFC.Repositories;
-
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
@@ -14,12 +19,33 @@ builder.Services.AddSwaggerGen(options =>
 
 builder.Services.AddControllers();
 
-// HttpClient para InventoryGateway
-builder.Services.AddHttpClient<InventoryHttpGateway>();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+if (string.IsNullOrEmpty(connectionString))
+    throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
-// DI explícita
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseMySQL(connectionString);
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowVueApp", policy =>
+        policy.WithOrigins("http://localhost:5176") 
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
+
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IInventoryGateway, InventoryHttpGateway>();
 builder.Services.AddScoped<SensorApplicationService>();
+builder.Services.AddHttpClient<InventoryHttpGateway>();
+builder.Services.AddScoped<ProductCommandService>();
+builder.Services.AddScoped<ProductQueryService>();
+builder.Services.AddScoped<SensorCommandService>();
+builder.Services.AddScoped<SensorQueryService>();
+
 
 var app = builder.Build();
 
@@ -31,5 +57,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
+app.UseCors("AllowVueApp");
 app.MapControllers();
 app.Run();
